e76b3940a2bd0e761911798e36d0062e30fc161a
Anders <Boefst@outlook.com>
2018-12-24 10:38:53 +0700

    Added and setup world0 API, updated nuget packages

---
+++ KinshipAPI/World0/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using APIHelper;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace World0 {
+    public class Program {
+        public static void Main(string[] args) {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls(new[] { "http://localhost:5002" })
+                .UseStartup<Startup>();
+    }
+}
